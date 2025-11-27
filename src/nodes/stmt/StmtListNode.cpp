c0f234e7cc7d44108e502431993fb5a1f5d4edbf
shustov-dmitry <shustov.dm.s@gmail.com>
2025-11-27 15:01:54 +0300

    feat: add realization of stmt list

--- src/nodes/stmt/StmtListNode.cpp
+++ src/nodes/stmt/StmtListNode.cpp
@@ -1,8 +1,29 @@
 #include "StmtListNode.h"
 
+#include "StmtNode.h"
+
 StmtListNode::StmtListNode(StmtNode *first) {
     stmts = new std::list<StmtNode*>();
     stmts->push_back(first);
 }
 
+StmtListNode * StmtListNode::addStmtToList(StmtListNode *list, StmtNode *stmt) {
+    if (list == nullptr) {
+        return new StmtListNode(stmt);
+    }
+
+    list->stmts->push_back(stmt);
+    return list;
+}
+
+StmtListNode * StmtListNode::addExprToStmtList(StmtListNode *list, ExprNode *expr) {
+    StmtNode* exprToStmt = StmtNode::createStmtFromExpr(expr);
+
+    if (list == nullptr) {
+        return new StmtListNode(exprToStmt);
+    }
+
+    list->stmts->push_back(exprToStmt);
+    return list;
+}
 
