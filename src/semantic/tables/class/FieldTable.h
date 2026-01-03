63a0e109a41dc08203b347ba0085c0eeba3ca4d7
Zinovev Ivan <izinovev77@gmail.com>
2026-01-03 12:49:04 +0300

    feat: FieldTable and FieldTableElements

---
+++ src/semantic/tables/class/FieldTable.h
@@ -0,0 +1,12 @@
+#ifndef KOTLIN_COMPILER_FIELDTABLE_H
+#define KOTLIN_COMPILER_FIELDTABLE_H
+#include <map>
+#include <string>
+#include "FieldTableElement.h"
+
+class FieldTable {
+public:
+    std::map<std::string, FieldTableElement*> fields;
+};
+
+#endif //KOTLIN_COMPILER_FIELDTABLE_H
---
+++ src/semantic/tables/class/FieldTableElement.h
@@ -0,0 +1,24 @@
+#ifndef KOTLIN_COMPILER_FIELDTABLEELEMENT_H
+#define KOTLIN_COMPILER_FIELDTABLEELEMENT_H
+#include <string>
+
+#include "ModifierMap.h"
+
+class FieldTableElement {
+public:
+    int name; // ссылка на константу с именем поля
+    int descriptor; // ссылка на константу с дескриптором поля
+    std::string strName;
+    std::string strDesc;
+    ModifierMap* modifierMap; // список модификаторов поля
+    int isConst; // является ли указанное поле изменяемым
+
+    FieldTableElement(int name,
+                      int descriptor,
+                      std::string strName,
+                      std::string strDesc,
+                      ModifierMap* modifierMap,
+                      bool isConst);
+};
+
+#endif //KOTLIN_COMPILER_FIELDTABLEELEMENT_H
