cbe10ae0df224dcf09c820fe708fceb351a71c9b
Zinovev Ivan <izinovev77@gmail.com>
2026-01-04 17:51:55 +0300

    feat: delete _

--- src/semantic/transformations/literals/LiteralsTransformator.cpp
+++ src/semantic/transformations/literals/LiteralsTransformator.cpp
@@ -1,57 +1,29 @@
 #include "LiteralsTransformator.h"
 
-static void _replaceLiteralsToObjectsInKotlinElement(KotlinFileNode * root);
-
-static void _replaceLiteralsToObjectsInKotlinElementList(KotlinElementList * root);
-
-static void _replaceLiteralsToObjectsInExpression(ExprNode * root);
-
-static void _replaceLiteralsToObjectsInExpressionList(ExprListNode * root);
-
-static void _replaceLiteralsToObjectsInStatement(StmtNode * root);
-
-static void _replaceLiteralsToObjectsInStatementList(StmtListNode * root);
-
-static void _replaceLiteralsToObjectsInFunction(FunNode * root);
-
-static void _replaceLiteralsToObjectsInClass(ClassNode * root);
-
-static void _replaceLiteralsToObjectsInClassBody(ClassBodyNode * root);
-
-static void _replaceLiteralsToObjectsInInheritance(Inheritance * root);
-
-static void _replaceLiteralsToObjectsInConstructor(Constructor * root);
-
-static void _replaceLiteralsToObjectsInConstructorArgs(ConstructorArgs * root);
-
-static void _replaceLiteralsToObjectsInVarDeclarationList(VarDeclarationList * root);
-
-static void _replaceLiteralsToObjectsInVarDeclaration(VarDeclaration * root);
-
-void replaceLiteralsToObjects(KotlinFileNode * root) {
-    _replaceLiteralsToObjectsInKotlinElement(root);
+void LiteralsTransformator::replaceLiteralsToObjects(KotlinFileNode * root) {
+    replaceLiteralsToObjectsInKotlinElement(root);
 }
 
-static void _replaceLiteralsToObjectsInKotlinElement(KotlinFileNode * root) {
-    _replaceLiteralsToObjectsInKotlinElementList(root->topLevelList);
+void LiteralsTransformator::replaceLiteralsToObjectsInKotlinElement(KotlinFileNode * root) {
+    replaceLiteralsToObjectsInKotlinElementList(root->topLevelList);
 }
 
-static void _replaceLiteralsToObjectsInKotlinElementList(KotlinElementList * root) {
+void LiteralsTransformator::replaceLiteralsToObjectsInKotlinElementList(KotlinElementList * root) {
     for (auto & cls : *root->classList) {
-        _replaceLiteralsToObjectsInClass(cls);
+        replaceLiteralsToObjectsInClass(cls);
     }
     for (auto & func : *root->functionList) {
-        _replaceLiteralsToObjectsInFunction(func);
+        replaceLiteralsToObjectsInFunction(func);
     }
 }
 
-static void _replaceLiteralsToObjectsInExpression(ExprNode * root) {
+void LiteralsTransformator::replaceLiteralsToObjectsInExpression(ExprNode * root) {
     if (root->type == _FUNC_CALL && root->params != nullptr) {
-        _replaceLiteralsToObjectsInExpressionList(root->params);
+        replaceLiteralsToObjectsInExpressionList(root->params);
     }
 
-    if (root->left != nullptr) _replaceLiteralsToObjectsInExpression(root->left);
-    if (root->right != nullptr) _replaceLiteralsToObjectsInExpression(root->right);
+    if (root->left != nullptr) replaceLiteralsToObjectsInExpression(root->left);
+    if (root->right != nullptr) replaceLiteralsToObjectsInExpression(root->right);
 
     if (root->type == _INTEGER_LITERAL) {
         root->type = _FUNC_CALL;
@@ -79,102 +51,102 @@ static void _replaceLiteralsToObjectsInExpression(ExprNode * root) {
         root->fromLiteral = _FROM_STRING;
     }
 
-    if (root->elements != nullptr) _replaceLiteralsToObjectsInExpressionList(root->elements);
+    if (root->elements != nullptr) replaceLiteralsToObjectsInExpressionList(root->elements);
 }
 
-static void _replaceLiteralsToObjectsInExpressionList(ExprListNode * root) {
+void LiteralsTransformator::replaceLiteralsToObjectsInExpressionList(ExprListNode * root) {
     for (auto& expr : *root->exprs ) {
-        _replaceLiteralsToObjectsInExpression(expr);
+        replaceLiteralsToObjectsInExpression(expr);
     }
 }
 
-static void _replaceLiteralsToObjectsInStatement(StmtNode * root) {
+void LiteralsTransformator::replaceLiteralsToObjectsInStatement(StmtNode * root) {
     switch (root->type) {
         case _IF_STMT:
-            _replaceLiteralsToObjectsInExpression(root->cond);
-            _replaceLiteralsToObjectsInStatementList(root->blockStmts);
+            replaceLiteralsToObjectsInExpression(root->cond);
+            replaceLiteralsToObjectsInStatementList(root->blockStmts);
             break;
         case _EXPRESSION:
-            _replaceLiteralsToObjectsInExpression(root->expr);
+            replaceLiteralsToObjectsInExpression(root->expr);
             break;
         case _WHILE:
-            _replaceLiteralsToObjectsInExpression(root->cond);
-            _replaceLiteralsToObjectsInStatementList(root->blockStmts);
+            replaceLiteralsToObjectsInExpression(root->cond);
+            replaceLiteralsToObjectsInStatementList(root->blockStmts);
             break;
         case _FOR:
-            _replaceLiteralsToObjectsInExpression(root->cond);
-            _replaceLiteralsToObjectsInStatementList(root->blockStmts);
+            replaceLiteralsToObjectsInExpression(root->cond);
+            replaceLiteralsToObjectsInStatementList(root->blockStmts);
             break;
         case _DO_WHILE:
-            _replaceLiteralsToObjectsInExpression(root->cond);
-            _replaceLiteralsToObjectsInStatementList(root->blockStmts);
+            replaceLiteralsToObjectsInExpression(root->cond);
+            replaceLiteralsToObjectsInStatementList(root->blockStmts);
             break;
         case _VAR:
-            _replaceLiteralsToObjectsInExpression(root->expr);
+            replaceLiteralsToObjectsInExpression(root->expr);
             break;
         case _VAL:
-            _replaceLiteralsToObjectsInExpression(root->expr);
+            replaceLiteralsToObjectsInExpression(root->expr);
             break;
         case _RETURN:
-            _replaceLiteralsToObjectsInExpression(root->expr);
+            replaceLiteralsToObjectsInExpression(root->expr);
             break;
         default: break;
     }
 }
 
-static void _replaceLiteralsToObjectsInStatementList(StmtListNode * root) {
+void LiteralsTransformator::replaceLiteralsToObjectsInStatementList(StmtListNode * root) {
     for (auto& stmt : *root->stmts ) {
-        _replaceLiteralsToObjectsInStatement(stmt);
+        replaceLiteralsToObjectsInStatement(stmt);
     }
 }
 
-static void _replaceLiteralsToObjectsInFunction(FunNode * root) {
-    if (root != nullptr) _replaceLiteralsToObjectsInStatementList(root->body);
+void LiteralsTransformator::replaceLiteralsToObjectsInFunction(FunNode * root) {
+    if (root != nullptr) replaceLiteralsToObjectsInStatementList(root->body);
 }
 
-static void _replaceLiteralsToObjectsInClass(ClassNode * root) {
+void LiteralsTransformator::replaceLiteralsToObjectsInClass(ClassNode * root) {
     /*TODO modifiers*/
-    _replaceLiteralsToObjectsInInheritance(root->inheritance);
-    _replaceLiteralsToObjectsInClassBody(root->body);
-    _replaceLiteralsToObjectsInConstructor(root->primaryConstructor);
+    replaceLiteralsToObjectsInInheritance(root->inheritance);
+    replaceLiteralsToObjectsInClassBody(root->body);
+    replaceLiteralsToObjectsInConstructor(root->primaryConstructor);
 }
 
-static void _replaceLiteralsToObjectsInInheritance(Inheritance * root) {
-    _replaceLiteralsToObjectsInExpressionList(root->constructorParams);
+void LiteralsTransformator::replaceLiteralsToObjectsInInheritance(Inheritance * root) {
+    replaceLiteralsToObjectsInExpressionList(root->constructorParams);
 }
 
-static void _replaceLiteralsToObjectsInClassBody(ClassBodyNode * root) {
+void LiteralsTransformator::replaceLiteralsToObjectsInClassBody(ClassBodyNode * root) {
     for (auto & constr : *root->constructors) {
-        _replaceLiteralsToObjectsInConstructor(constr);
+        replaceLiteralsToObjectsInConstructor(constr);
     }
-    _replaceLiteralsToObjectsInStatementList(root->fields);
+    replaceLiteralsToObjectsInStatementList(root->fields);
     for (auto & method : *root->methods) {
-        _replaceLiteralsToObjectsInFunction(method);
+        replaceLiteralsToObjectsInFunction(method);
     }
 }
 
-static void _replaceLiteralsToObjectsInConstructor(Constructor * root) {
+void LiteralsTransformator::replaceLiteralsToObjectsInConstructor(Constructor * root) {
     /*TODO modifiers prevConstructorType prevConstructorArgs*/
-    _replaceLiteralsToObjectsInConstructorArgs(root->args);
-    _replaceLiteralsToObjectsInStatementList(root->stmts);
+    replaceLiteralsToObjectsInConstructorArgs(root->args);
+    replaceLiteralsToObjectsInStatementList(root->stmts);
 }
 
-static void _replaceLiteralsToObjectsInConstructorArgs(ConstructorArgs * root) {
+void LiteralsTransformator::replaceLiteralsToObjectsInConstructorArgs(ConstructorArgs * root) {
     for (auto & arg : root->args) {
         if (VarDeclaration * varDecl = dynamic_cast<VarDeclaration*>(arg)) {
-            _replaceLiteralsToObjectsInVarDeclaration(varDecl);
+            replaceLiteralsToObjectsInVarDeclaration(varDecl);
         } else if (StmtNode * stmt = dynamic_cast<StmtNode*>(arg)) {
-            _replaceLiteralsToObjectsInStatement(stmt);
+            replaceLiteralsToObjectsInStatement(stmt);
         }
     }
 }
 
-static void _replaceLiteralsToObjectsInVarDeclarationList(VarDeclarationList * root) {
+void LiteralsTransformator::replaceLiteralsToObjectsInVarDeclarationList(VarDeclarationList * root) {
     for (auto & decl : *root->decls) {
-        _replaceLiteralsToObjectsInVarDeclaration(decl);
+        replaceLiteralsToObjectsInVarDeclaration(decl);
     }
 }
 
-static void _replaceLiteralsToObjectsInVarDeclaration(VarDeclaration * root) {
+void LiteralsTransformator::replaceLiteralsToObjectsInVarDeclaration(VarDeclaration * root) {
     /*TODO не знаю*/
 }
